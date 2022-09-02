7ad984809c069d792fcca0fb694260b49a784f40
ondicho <ondicho53@gmail.com>
2022-09-02 12:54:03 +0300

    add responsiveness to landing page

--- src/components/Bills.js
+++ src/components/Bills.js
@@ -3,6 +3,9 @@ import Rent from "../images/Rent.jpg";
 import "../components/style.css";
 import MyNavbar from "./Navbar";
 import Footer from "./Footer";
+import Water from "../images/water-tap.png";
+import Elec from "../images/idea.png";
+import rent from "../images/rent.png";
 
 function Bills() {
   Cookies.set("isLoggedIn", "false");
@@ -11,7 +14,31 @@ function Bills() {
   return (
     <>
       <MyNavbar />
-      <img className="bills" src={Rent} />
+      {/* <img className="bills" src={Rent} /> */}
+
+      <div className="landing">
+        <div sm={6} className="landing-col services-main">
+
+          <div className="services-container">
+            <div className="services">
+              <img src={rent} alt="rent" height="150px" />
+            </div>
+            <div className=" services">
+              <img src={Water} alt="water" height="150px" />
+            </div>
+            <div className="services">
+              <img src={Elec} alt="electricity" height="150px" />
+            </div>
+          </div>
+          <div className="services-description">
+            <p> Pay rent and other bills at the click of a button.</p>
+          </div>
+        </div>
+        <div sm={6} className="landing-col image">
+          <img className="main-image" src={Rent} />
+        </div>
+      </div>
+
       <Footer />
     </>
   );
