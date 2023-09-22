a1ec448a339dd152404618b772052431579c1ba9
StevenMo <stevenmolvin@gmail.com>
2023-09-22 03:10:58 +0300

    Feat: Modified footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,33 +8,23 @@ function Footer() {
       <FooterContainer className="main-footer">
         <div className="footer-middle">
           <div className="container">
-            <div className="row">
-              <div className="col-md-3 col-sm-6">
-                <h4>Quick Links</h4>
-                <ul className="list-unstyled">
-                  <li>Home</li>
-                  <li>Make Payments</li>
-                  <li>Raise Ticket</li>
-                </ul>
-              </div>
-              <div className="col-md-3 col-sm-6">
-                <h4>Quick Links</h4>
-                <ul className="list-unstyled">
-                  <li>Home</li>
-                  <li>Make Payments</li>
-                  <li>Raise Ticket</li>
-                </ul>
-              </div>
-              <div className="col-md-3 col-sm-6">
-                <h4>Quick Links</h4>
-                <ul className="list-unstyled">
-                  <li>Home</li>
-                  <li>Make Payments</li>
-                  <li>Raise Ticket</li>
-                </ul>
-              </div>
+          <div className="row">
+            <div className="col-md-3 col-sm-6">
+              <h4>Quick Links</h4>
+              <ul className="list-unstyled">
+                <li>
+                  <StyledLink to="/">Home</StyledLink>
+                </li>
+                <li>
+                  <StyledLink to="/payments">Make Payments</StyledLink>
+                </li>
+                <li>
+                  <StyledLink to="/tickets">Raise Ticket</StyledLink>
+                </li>
+              </ul>
             </div>
-            <div className="footer-bottom">
+          </div>
+          <div className="footer-bottom">
               <p className="text-xs-center">
                 &copy;{new Date().getFullYear()} Copyright : Albert Ondicho
               </p>
